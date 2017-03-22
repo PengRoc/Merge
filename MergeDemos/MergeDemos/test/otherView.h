1bb3567488484d21d143da4eb2b340b7f4f37dd2
Peng <1498284491@qq.com>
2017-03-22 15:11:30 +0800

    add otherview

---
+++ MergeDemos/MergeDemos/test/otherView.h
@@ -0,0 +1,13 @@
+//
+//  otherView.h
+//  MergeDemos
+//
+//  Created by 云 Love on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface otherView : UIView
+
+@end
---
+++ MergeDemos/MergeDemos/test/otherView.m
@@ -0,0 +1,21 @@
+//
+//  otherView.m
+//  MergeDemos
+//
+//  Created by 云 Love on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import "otherView.h"
+
+@implementation otherView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

1b39e929d5f46e14b320c43121923fa040c766be
sanpen <sanpeng.zhang@xfunsun.com>
2017-03-22 15:09:11 +0800

    add testview

---
+++ MergeDemos/MergeDemos/testView.m
@@ -0,0 +1,21 @@
+//
+//  testView.m
+//  MergeDemos
+//
+//  Created by 鑫方迅 on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

4154b6782758a63245a5e8d43b99eb6d951c997f
Peng <1498284491@qq.com>
2017-03-22 15:07:01 +0800

    add new view

---
+++ MergeDemos/MergeDemos/newView.h
@@ -0,0 +1,13 @@
+//
+//  newView.h
+//  MergeDemos
+//
+//  Created by 云 Love on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newView : UIView
+
+@end

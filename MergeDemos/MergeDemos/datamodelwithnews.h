41302d9ca8cbb271a11bcc480037bbb32869421b
Peng <1498284491@qq.com>
2017-03-23 00:47:16 +0800

    newfile

---
+++ MergeDemos/MergeDemos/datamodelwithnews.h
@@ -0,0 +1,13 @@
+//
+//  datamodelwithnews.h
+//  MergeDemos
+//
+//  Created by 云 Love on 2017/3/23.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface datamodelwithnews : NSObject
+
+@end
---
+++ MergeDemos/MergeDemos/datamodelwithnews.m
@@ -0,0 +1,13 @@
+//
+//  datamodelwithnews.m
+//  MergeDemos
+//
+//  Created by 云 Love on 2017/3/23.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import "datamodelwithnews.h"
+
+@implementation datamodelwithnews
+
+@end

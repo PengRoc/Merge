aef73d7b14619103e41db1c58e626530a6ef6f71
sanpen <sanpeng.zhang@xfunsun.com>
2017-03-22 15:32:10 +0800

    add a operationView

---
+++ MergeDemos/MergeDemos/test/OperationView.h
@@ -0,0 +1,13 @@
+//
+//  OperationView.h
+//  MergeDemos
+//
+//  Created by 鑫方迅 on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OperationView : UIView
+
+@end
---
+++ MergeDemos/MergeDemos/test/OperationView.m
@@ -0,0 +1,21 @@
+//
+//  OperationView.m
+//  MergeDemos
+//
+//  Created by 鑫方迅 on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import "OperationView.h"
+
+@implementation OperationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
